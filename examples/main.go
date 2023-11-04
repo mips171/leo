de734f314d2363dfa7257b4b5049f54ae9f554d9
Nicholas Smith <nicholas@nbembedded.com>
2023-11-05 08:06:49 +1000

    tabs to spaces
    Signed-off-by: Nicholas Smith <nicholas@nbembedded.com>

--- examples/main.go
+++ examples/main.go
@@ -10,46 +10,46 @@ import (
 
 func main() {
 
-	// Step 1. Create a new task tasksGraph
-	tasksGraph := leo.TaskGraph()
+    // Step 1. Create a new task tasksGraph
+    tasksGraph := leo.TaskGraph()
 
-	// Define a simple task function that simulates work by sleeping for a random duration
-	taskFunc := func(name string) leo.TaskFunc {
-		return func() error {
-			fmt.Printf("Executing task %s\n", name)
-			// Simulate work with a random sleep
-			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-			fmt.Printf("Completed task %s\n", name)
-			return nil
-		}
-	}
+    // Define a simple task function that simulates work by sleeping for a random duration
+    taskFunc := func(name string) leo.TaskFunc {
+        return func() error {
+            fmt.Printf("Executing task %s\n", name)
+            // Simulate work with a random sleep
+            time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+            fmt.Printf("Completed task %s\n", name)
+            return nil
+        }
+    }
 
-	// Step 2. Add tasks to the graph. Each task is just a function that prints its name and sleeps.
-	tasksGraph.Add("Task A", taskFunc("Task A"))
-	tasksGraph.Add("Task B", taskFunc("Task B"))
-	tasksGraph.Add("Task C", taskFunc("Task C"))
-	tasksGraph.Add("Task D", taskFunc("Task D"))
+    // Step 2. Add tasks to the graph. Each task is just a function that prints its name and sleeps.
+    tasksGraph.Add("Task A", taskFunc("Task A"))
+    tasksGraph.Add("Task B", taskFunc("Task B"))
+    tasksGraph.Add("Task C", taskFunc("Task C"))
+    tasksGraph.Add("Task D", taskFunc("Task D"))
 
-	// Step 3. Establish dependencies: Task A must precede Task B and Task C. Task D succeeds task B and C.
-	// This means that Task B and Task C will run in parallel after Task A completes,
-	// and Task D will run after both Task B and Task C complete.
-	tasksGraph.Precede("Task A", "Task B") // A runs before B
-	tasksGraph.Precede("Task A", "Task C") // A also runs before C
-	tasksGraph.Succeed("Task D", "Task B") // D runs after B
-	tasksGraph.Succeed("Task D", "Task C") // D also runs after C
+    // Step 3. Establish dependencies: Task A must precede Task B and Task C. Task D succeeds task B and C.
+    // This means that Task B and Task C will run in parallel after Task A completes,
+    // and Task D will run after both Task B and Task C complete.
+    tasksGraph.Precede("Task A", "Task B") // A runs before B
+    tasksGraph.Precede("Task A", "Task C") // A also runs before C
+    tasksGraph.Succeed("Task D", "Task B") // D runs after B
+    tasksGraph.Succeed("Task D", "Task C") // D also runs after C
 
-	// Optionally print the graph to help with debugging
-	tasksGraph.Print()
+    // Optionally print the graph to help with debugging
+    tasksGraph.Print()
 
-	// Step 4. Create an executor to run the graph
-	executor := leo.NewExecutor(tasksGraph)
-	fmt.Println("Executing graph in a loop...")
-	for i := 0; i < 3; i++ {
-		// Step 5. Execute the graph. This will run Task A first, then Task B and Task C in parallel, then task D after B and C
-		if err := executor.Execute(); err != nil {
-			fmt.Printf("Execution failed: %v\n", err)
-		} else {
-			fmt.Println("All tasks executed successfully.")
-		}
-	}
+    // Step 4. Create an executor to run the graph
+    executor := leo.NewExecutor(tasksGraph)
+    fmt.Println("Executing graph in a loop...")
+    for i := 0; i < 3; i++ {
+        // Step 5. Execute the graph. This will run Task A first, then Task B and Task C in parallel, then task D after B and C
+        if err := executor.Execute(); err != nil {
+            fmt.Printf("Execution failed: %v\n", err)
+        } else {
+            fmt.Println("All tasks executed successfully.")
+        }
+    }
 }
--- leo.go
+++ leo.go
@@ -1,191 +1,191 @@
 package leo
 
 import (
-	"errors"
-	"fmt"
-	"sync"
+    "errors"
+    "fmt"
+    "sync"
 )
 
 type TaskFunc func() error
 
 type Node struct {
-	task     TaskFunc
-	children []*Node
-	parents  []*Node
-	name     string
+    task     TaskFunc
+    children []*Node
+    parents  []*Node
+    name     string
 }
 
 type Graph struct {
-	nodes      map[string]*Node
-	startNodes []*Node
+    nodes      map[string]*Node
+    startNodes []*Node
 }
 
 func TaskGraph() *Graph {
-	return &Graph{
-		nodes: make(map[string]*Node),
-	}
+    return &Graph{
+        nodes: make(map[string]*Node),
+    }
 }
 
 func (g *Graph) Add(name string, task TaskFunc) {
-	if _, exists := g.nodes[name]; !exists {
-		g.nodes[name] = &Node{
-			task:     task,
-			children: make([]*Node, 0),
-			parents:  make([]*Node, 0),
-			name:     name,
-		}
-		g.startNodes = append(g.startNodes, g.nodes[name])
-	}
+    if _, exists := g.nodes[name]; !exists {
+        g.nodes[name] = &Node{
+            task:     task,
+            children: make([]*Node, 0),
+            parents:  make([]*Node, 0),
+            name:     name,
+        }
+        g.startNodes = append(g.startNodes, g.nodes[name])
+    }
 }
 
 // Precede adds a directed edge from node `from` to node `to`
 func (g *Graph) Precede(from, to string) error {
-	fromNode, fromExists := g.nodes[from]
-	toNode, toExists := g.nodes[to]
+    fromNode, fromExists := g.nodes[from]
+    toNode, toExists := g.nodes[to]
 
-	if !fromExists || !toExists {
-		return errors.New("one or both nodes do not exist")
-	}
+    if !fromExists || !toExists {
+        return errors.New("one or both nodes do not exist")
+    }
 
-	fromNode.children = append(fromNode.children, toNode)
-	toNode.parents = append(toNode.parents, fromNode)
+    fromNode.children = append(fromNode.children, toNode)
+    toNode.parents = append(toNode.parents, fromNode)
 
-	if g.hasCycle() {
-		fromNode.children = fromNode.children[:len(fromNode.children)-1]
-		toNode.parents = toNode.parents[:len(toNode.parents)-1]
-		return errors.New("adding this edge would create a cycle")
-	}
+    if g.hasCycle() {
+        fromNode.children = fromNode.children[:len(fromNode.children)-1]
+        toNode.parents = toNode.parents[:len(toNode.parents)-1]
+        return errors.New("adding this edge would create a cycle")
+    }
 
-	return nil
+    return nil
 }
 
 // Succeed sets up a "succeeds" relationship, indicating that `to` should succeed `from`.
 func (g *Graph) Succeed(from, to string) error {
-	return g.Precede(to, from)
+    return g.Precede(to, from)
 }
 
 type Executor struct {
-	graph *Graph
+    graph *Graph
 }
 
 func NewExecutor(graph *Graph) *Executor {
-	parentCounts := make(map[*Node]int)
-	for _, node := range graph.nodes {
-		for _, child := range node.children {
-			parentCounts[child]++
-		}
-	}
+    parentCounts := make(map[*Node]int)
+    for _, node := range graph.nodes {
+        for _, child := range node.children {
+            parentCounts[child]++
+        }
+    }
 
-	for node, count := range parentCounts {
-		node.parents = make([]*Node, 0, count)
-	}
+    for node, count := range parentCounts {
+        node.parents = make([]*Node, 0, count)
+    }
 
-	for _, node := range graph.nodes {
-		for _, child := range node.children {
-			child.parents = append(child.parents, node)
-		}
-	}
+    for _, node := range graph.nodes {
+        for _, child := range node.children {
+            child.parents = append(child.parents, node)
+        }
+    }
 
-	return &Executor{
-		graph: graph,
-	}
+    return &Executor{
+        graph: graph,
+    }
 }
 
 func (e *Executor) Execute() error {
-	var wg sync.WaitGroup
-	inDegree := make(map[*Node]int)
-	ready := make(chan *Node, len(e.graph.nodes))
-	errors := make(chan error, 1)
-	finished := make(chan struct{})
+    var wg sync.WaitGroup
+    inDegree := make(map[*Node]int)
+    ready := make(chan *Node, len(e.graph.nodes))
+    errors := make(chan error, 1)
+    finished := make(chan struct{})
 
-	for _, node := range e.graph.nodes {
-		inDegree[node] = len(node.parents)
-		if inDegree[node] == 0 {
-			wg.Add(1)
-			go func(n *Node) {
-				ready <- n
-			}(node)
-		}
-	}
+    for _, node := range e.graph.nodes {
+        inDegree[node] = len(node.parents)
+        if inDegree[node] == 0 {
+            wg.Add(1)
+            go func(n *Node) {
+                ready <- n
+            }(node)
+        }
+    }
 
-	go func() {
-		wg.Wait()
-		close(finished)
-	}()
+    go func() {
+        wg.Wait()
+        close(finished)
+    }()
 
-	go func() {
-		for node := range ready {
-			go func(n *Node) {
-				defer wg.Done()
-				if err := n.task(); err != nil {
-					select {
-					case errors <- fmt.Errorf("error executing node %s: %w", n.name, err):
-					default:
-						// If an error is already recorded, we ignore subsequent errors
-					}
-					return
-				}
+    go func() {
+        for node := range ready {
+            go func(n *Node) {
+                defer wg.Done()
+                if err := n.task(); err != nil {
+                    select {
+                    case errors <- fmt.Errorf("error executing node %s: %w", n.name, err):
+                    default:
+                        // If an error is already recorded, we ignore subsequent errors
+                    }
+                    return
+                }
 
-				for _, child := range n.children {
-					inDegree[child]--
-					if inDegree[child] == 0 {
-						wg.Add(1)
-						ready <- child
-					}
-				}
-			}(node)
-		}
-	}()
+                for _, child := range n.children {
+                    inDegree[child]--
+                    if inDegree[child] == 0 {
+                        wg.Add(1)
+                        ready <- child
+                    }
+                }
+            }(node)
+        }
+    }()
 
-	select {
-	case <-finished:
-		return nil
-	case err := <-errors:
-		return err
-	}
+    select {
+    case <-finished:
+        return nil
+    case err := <-errors:
+        return err
+    }
 }
 
 func (g Graph) Print() {
-	for _, node := range g.nodes {
-		fmt.Printf("%s -> ", node.name)
-		for _, child := range node.children {
-			fmt.Printf("%s, ", child.name)
-		}
-		fmt.Println()
-	}
+    for _, node := range g.nodes {
+        fmt.Printf("%s -> ", node.name)
+        for _, child := range node.children {
+            fmt.Printf("%s, ", child.name)
+        }
+        fmt.Println()
+    }
 }
 
 func (g *Graph) dfsCheckCycle(node *Node, visited, recStack map[*Node]bool) bool {
-	if !visited[node] {
+    if !visited[node] {
 
-		visited[node] = true
-		recStack[node] = true
+        visited[node] = true
+        recStack[node] = true
 
-		for _, child := range node.children {
-			if !visited[child] && g.dfsCheckCycle(child, visited, recStack) {
-				return true
-			} else if recStack[child] {
-				return true
-			}
-		}
-	}
+        for _, child := range node.children {
+            if !visited[child] && g.dfsCheckCycle(child, visited, recStack) {
+                return true
+            } else if recStack[child] {
+                return true
+            }
+        }
+    }
 
-	recStack[node] = false
-	return false
+    recStack[node] = false
+    return false
 }
 
 // hasCycle checks if there would be a cycle created by adding an edge from `from` to `to`
 func (g *Graph) hasCycle() bool {
-	visited := make(map[*Node]bool)
-	recStack := make(map[*Node]bool)
+    visited := make(map[*Node]bool)
+    recStack := make(map[*Node]bool)
 
-	for _, node := range g.nodes {
-		if !visited[node] {
-			if g.dfsCheckCycle(node, visited, recStack) {
-				return true
-			}
-		}
-	}
+    for _, node := range g.nodes {
+        if !visited[node] {
+            if g.dfsCheckCycle(node, visited, recStack) {
+                return true
+            }
+        }
+    }
 
-	return false
+    return false
 }
